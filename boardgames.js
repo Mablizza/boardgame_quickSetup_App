b62da12660369a8677ace9d3f01967470968c452
Mablizza <152658473+Mablizza@users.noreply.github.com>
2024-12-18 11:20:36 -0600

    Initial Commit
    Loading initial prototype app

---
+++ boardgames.js
@@ -0,0 +1,97 @@
+export let bgCollection = [
+    {title: "Wasteland Express",
+    img: "https://cf.geekdo-images.com/s6yFAO5N9PIJsfqp7WMgyw__itemrep/img/mfloQeHchJrg4vFDaCgUX9yt90k=/fit-in/246x300/filters:strip_icc()/pic3612509.png",
+    number_of_players: "2-5",
+    details: {
+        board_setup: 
+            {
+            0: "Shuffle the 16 terrain octagons and distribute them randomly in a 4x4 grid to create the wasteland map.",
+            1: "Place 'The Depot' tile in the center.",
+            2: "Place the 4 raider enclaves on the edges of the map with 'Railmen'and 'Nein Nein' directly opposite one another.",
+            3: "Randomly distribute the other 16 outposts",
+            4: "Place the 3 raider trucks randomly in 3 different corner terrain octagons",
+            5: "Establish the Cargo Market, Contracts & Events",
+            6: "Players choose their Character board"
+            },
+        player_setup: 
+            {
+            0: "Give each player $10 in $crap.",
+            1: "1 general storage, 1 weapons storage and their respective character board pieces",
+            2: "Place Player's trcuks in 'The Depot'.",
+            3: "Put the 5 action markers in the [AVAILABLE] section of each player’s dashboard.",
+            4: "Each player draws 1 card from each faction job deck (i.e., 3 total) and chooses 1 to keep; the other 2 are discarded, face down, to the bottom of their respective decks.",
+            5: "Check and recieve your unique character bonus."
+            },
+        player_turns: 
+            {
+            0: "On your turn, you take one action of your choice (or pass). To take an action, you move an unused action marker from [AVAILABLE] to the corresponding section of your dashboard. The number of actions you can take each round is limited to the number of action markers you have, and certain actions have a maximum number of activations per round; you cannot take an action if you do not have an available slot in that section of your dashboard.",
+            1: "There are 6 available actions: [Move Your Truck] [Attack Raiders] [Purchase Cargo] [Deliver Cargo] [Activate an Outpost] [Bonus Action]",
+            2: "The end of a round occurs once all players have taken all their actions. At that point, all players reset their action markers to [AVAILABLE], the event deck is passed clockwise, and a new round begins. The new player in possession of the events deck will be start player that round."
+            },
+        win_condition: "The first player to complete 3 contracts—in any combination of public and private contracts—wins the game!"
+        },
+    pdf_rulebook: "https://fgbradleys.com/wp-content/uploads/rules/Wasteland%20Express%20Delivery%20Service%20-%20rules.pdf"
+    },
+    {title: "Clank! A Deck-Building Adventure",
+        img: "https://cf.geekdo-images.com/DPjV1iI0ygo5Bl3XLNRiIg__imagepage/img/R9z8w4GndbY-r3fG_LK1LvBvLc0=/fit-in/900x600/filters:no_upscale():strip_icc()/pic4449526.jpg",
+        number_of_players: "2-4",
+        details: {
+            board_setup: 
+                {
+                0: "Separate the 3 different kinds of cards. [Gray] [Blue] [Yellow]",
+                1: "Put the Dragon token on the Dragon Track and all dragon cubes (24) on the bag.",
+                2: "Randomly put the Major Secrets and Minor Secrets face down on the board (9 areas).",
+                3: "Place Artifacts, Monkey Idols and other respective Market tokens. ",
+                4: "Place each player’s Meeple on the flag in the top left along with Mastery Tokens. ",
+                5: "Place 4 Reserve Card in a row. [Explore, Merc, Tome & Goblin]",
+                6: "Place 6 Market cards below the board in a row, remove any with the Dragon symbol"
+                },
+            player_setup: 
+                {
+                0: "Give each player 6 Burgles, 2 Stumbles, 1 Sidestep & 1 Scramble. ",
+                1: "Sneakiest player goes first. First player puts 3 Clank! cubes in the Cache, second player puts 2 cubes and so on..."
+                },
+            player_turns: 
+                {
+                0: "Replenish the Dungeon Card Row",
+                1: "Deal dragon damage if applicable (put all cached cubes in the bag, and draw equal to dragon counter.",
+                2: "Each turn you use your hand of 5 cards to determine your actions. [Move, Buy, Attack & Abilities]"
+                },
+            win_condition: "End game is triggered when the first person makes it back to the start (flag in top left) with an Artifact. When all other players are knocked out or leave the dungeon, you can proceed to count Victory Points. Plays with most VP wins!"
+            },
+        pdf_rulebook: ""
+        },
+        {title: "Clank! in! Space!",
+            img: "https://alwaysgames.ca/cdn/shop/files/PXL_20231109_215309551_2_1024x.jpg",
+            number_of_players: "2-4",
+            details: {
+                board_setup: 
+                    {
+                    0: "Assemble the board as desired. (only 3 middle pieces can vary)",
+                    1: "Put all boss cubes (24) in the bag and set Rage Track by adding red Hunter cubes in corresponging space, adding Blockade token on the last space of the red zone and placing Boss marking in corresponding spot (depending on player count).",
+                    2: "Randomly put the Major Secrets and Minor Secrets face down on the board (9 areas).",
+                    3: "Place Artifacts at random. (3 player, remove 1 Artifact | 2 player, remove 2 Artifacts)",
+                    4: "Place each player´s pawn on the Cargo Module starting space. ",
+                    5: "Place 4 Reserve Card in a row. [FAZR, Boldly Go, Core & G0b-1in]",
+                    6: "Place 6 Adventure cards below the board in a row, replace any with the Boss symbol.",
+                    7: "Set up the Market next to the board. [Contraband] [Master Key] [Med Kit] [TelePass]"
+                    },
+                player_setup: 
+                    {
+                    0: "Each player chooses a pawn, 30 Clank cubes, 2 Data cubes",
+                    1: "Give each player starting deck of 6 Hacks, 2 Stumbles, 1 Access & 1 Scramble. ",
+                    2: "Sneakiest player goes first. First player puts 3 Clank! cubes in the Cache, second player puts 2 cubes and so on..."
+                    },
+                player_turns: 
+                    {
+                    0: "Replenish the Dungeon Card Row",
+                    1: "Deal Boss damage if applicable (put all cached cubes in the bag, and draw equal to Boss counter.",
+                    2: "Each turn you use your hand of 5 cards to determine your actions. [Move, Buy, Attack & Abilities]"
+                    },
+                win_condition: "You cannnot grab an Artifact or enter the Command Room without first Hacking two Access Points. End game is triggered with the first player to take an Escape Pod with an Artifact. When all other players are knocked out or leave the dungeon, you can proceed to count Victory Points. Plays with most VP wins!"
+                },
+            pdf_rulebook: ""
+            }
+]
+
+//https://fgbradleys.com/wp-content/uploads/rules/Wasteland%20Express%20Delivery%20Service%20-%20rules.pdf
---
+++ index.js
@@ -0,0 +1,165 @@
+import { bgCollection } from "./boardgames.js"
+
+const pageTitle = document.getElementById("page-title")
+const searchContainer = document.getElementById("search-container")
+const cardContainerDiv = document.getElementById("card-container")
+const detailsContainer = document.getElementById("details-container")
+const heroContainer = document.getElementById("hero")
+
+let selectedTitle = ""
+let searchedBG = ""
+let filteredBGArray = []
+
+function addSearchFunctionality(){
+    const inputEle = document.getElementById("input-element")
+    const searchBtn = document.getElementById("search-btn")
+    
+    inputEle.value = ""
+
+    searchBtn.addEventListener("click", function(){
+        searchedBG = inputEle.value
+        filteredBGArray = bgCollection.filter((boardgame) => boardgame.title.includes(searchedBG))
+        generateSummaryCardsArray(filteredBGArray)
+    })
+}
+addSearchFunctionality()
+
+function generateSummaryCardsArray(bgArray){
+    let idGenerator = 1
+
+    cardContainerDiv.innerHTML = ``
+    bgArray.forEach(boardgame => {
+        cardContainerDiv.innerHTML += `                                  
+                                        <div class="summary-card" id="id-0${idGenerator}">
+                                            <h3> ${boardgame.title} </h3>
+                                            <image src="${boardgame.img}" alt="${boardgame.title} sample image">
+                                            <dl>
+                                                <dt>Number of Players: <dt>
+                                                <dd>${boardgame.number_of_players}
+                                            </dl>
+                                        </div>
+                                    `
+        idGenerator++
+    });
+}
+generateSummaryCardsArray(bgCollection)
+
+function addClickListenerToCards() {
+    //adds an event listener for click to newly created cards
+        const classSummaryCardObject = document.getElementsByClassName("summary-card")
+
+        for (let i=classSummaryCardObject.length-1; i>-1; i--){
+            //loop through all the cards with class("summary-cards")
+            addEventListener("click", cardClick)
+        }
+
+        function cardClick(e){
+            let clickedID = ""
+
+            console.log(e.target.parentElement.id)
+            if(e.target.parentElement.id.includes("id")){
+                clickedID = e.target.parentElement.id
+                console.log("Clicked Card ID:", clickedID)
+            }
+            else if ( e.target.parentElement.id == "card-container" ) {
+                console.log("outer Elem, going on node down for ID", e.target.parentElement.id)
+                clickedID = e.target.id
+                console.log("Clicked Card ID:", clickedID)
+            } 
+            else {
+                console.log("no id, going up a level for ID", e.target.parentElement.id)
+                clickedID = e.target.parentElement.parentElement.id
+                console.log("Clicked Card ID:", clickedID)
+            }
+            
+            const clickedCardElement = document.getElementById(clickedID)
+            if (clickedCardElement == null) {return}
+            else {
+                console.log(clickedCardElement.children[0].innerText)
+                selectedTitle = clickedCardElement.children[0].innerText
+            }
+        renderTitle(selectedTitle)
+        searchContainer.style.display = "none"  // display = "block" to revert
+        displayCardDetails()
+        }
+}
+addClickListenerToCards()
+
+
+function renderTitle(title = "Quick Game Setup Guide"){
+    pageTitle.innerText = title
+}
+renderTitle()
+
+
+function displayCardDetails(){
+    const selectedBoardGame = bgCollection.find((boardgame) => boardgame.title === selectedTitle)
+    const selectedBGDetails = Object.keys(selectedBoardGame.details)
+    const selectedBGDetailsValues = Object.entries(selectedBoardGame.details)    
+    
+    function createDivContainersForBGDetails(){    
+        for (const key in selectedBGDetails) {
+            //create a div for each category of details existing in the boardgame Object
+            detailsContainer  //.appendChild(document.createElement("div")).id = selectedBGDetails[key]
+                .appendChild(document.createElement("div"))
+                .setAttribute("id", selectedBGDetails[key])
+        }
+    }
+    createDivContainersForBGDetails()
+    
+    heroContainer.innerHTML = `<img src="${selectedBoardGame.img}">`
+
+    function renderBoardSetup(){
+        const boardSetUpContainer = document.getElementById("board_setup")
+        boardSetUpContainer.appendChild(document.createElement("h2")).textContent = `${selectedBGDetailsValues[0][0]}`
+        const boardSetupOL = boardSetUpContainer.appendChild(document.createElement("ol"))
+        for (const value in selectedBGDetailsValues[0][1]) {
+            boardSetupOL.appendChild(document.createElement("li")).textContent = `${selectedBGDetailsValues[0][1][value]}`
+        }
+    }
+    renderBoardSetup()
+
+    function renderPlayerSetup(){
+        const playerSetUpContainer = document.getElementById("player_setup")
+        playerSetUpContainer.appendChild(document.createElement("h2")).textContent = `${selectedBGDetailsValues[1][0]}`
+        const playerSetupOL = playerSetUpContainer.appendChild(document.createElement("ol"))
+        for (const value in selectedBGDetailsValues[1][1]) {
+            playerSetupOL.appendChild(document.createElement("li")).textContent = `${selectedBGDetailsValues[1][1][value]}`
+        }
+    }
+    renderPlayerSetup()
+
+    function renderPlayerTurns(){
+        const playerTurnsContainer = document.getElementById("player_turns")
+        playerTurnsContainer.appendChild(document.createElement("h2")).textContent = `${selectedBGDetailsValues[2][0]}`
+        const playerTurnsOL = playerTurnsContainer.appendChild(document.createElement("ol"))
+        for (const value in selectedBGDetailsValues[2][1]) {
+            playerTurnsOL.appendChild(document.createElement("li")).textContent = `${selectedBGDetailsValues[2][1][value]}`
+        }
+    }
+    renderPlayerTurns()
+    
+    function renderWinCondition(){
+        const winConditionContainer = document.getElementById("win_condition")
+    
+        winConditionContainer.appendChild(document.createElement("h2")).textContent = `${selectedBGDetailsValues[3][0]}`
+        winConditionContainer.appendChild(document.createElement("p")).textContent = `${selectedBGDetailsValues[3][1]}`
+    }
+    renderWinCondition()
+
+    function renderPDFLinkIfAvailable(){    
+    //if PDF link is available, render the PDF as an Anchor Link
+        if (selectedBoardGame.pdf_rulebook == null) {
+            console.log("there is no available pdf to show")
+        } else if (selectedBoardGame.pdf_rulebook !== "") {
+            console.log("there is an available pdf to show!")
+            //render the PDF as an Anchor Link
+            detailsContainer.appendChild(document.createElement("div")).id = "pdf-container"
+            document.getElementById("pdf-container").innerHTML = `
+                                                                <a id="pdf-link" href="${selectedBoardGame.pdf_rulebook}" target="_blank">SEE PDF RULEBOOK </a>
+                                                                `
+        } else {console.log("there is no available pdf to show")}
+    }  
+    renderPDFLinkIfAvailable()
+
+}
